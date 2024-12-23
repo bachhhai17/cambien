5e4a60b6ea042f6d3bf8ee58490c2668bb5b4884
bachhai17 <mittotone@gmail.com>
2024-12-23 23:11:10 +0700

    cambien

--- cambiennhietdo.ino
+++ cambiennhietdo.ino
@@ -1,55 +1,71 @@
+#define BLYNK_TEMPLATE_ID "TMPL6kwY-y_AW"
+#define BLYNK_TEMPLATE_NAME "cambien"
+#define BLYNK_AUTH_TOKEN "ltl1bUoUg7X75uqYYLaiWh0d8Arlf_0W"
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
+#include <string.h>
+
 #include "DHT.h"
-#define DHTPIN 4     // Digital pin connected to the DHT sensor
-// Feather HUZZAH ESP8266 note: use pins 3, 4, 5, 12, 13 or 14 --
-// Pin 15 can work but DHT must be disconnected during program upload.
-// Uncomment whatever type you're using!
-//#define DHTTYPE DHT11   // DHT 11
-#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
-//#define DHTTYPE DHT21   // DHT 21 (AM2301)
-// Connect pin 1 (on the left) of the sensor to +5V
-// NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
-// to 3.3V instead of 5V!
-// Connect pin 2 of the sensor to whatever your DHTPIN is
-// Connect pin 4 (on the right) of the sensor to GROUND
-// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
-// Initialize DHT sensor.
-// Note that older versions of this library took an optional third parameter to
-// tweak the timings for faster processors.  This parameter is no longer needed
-// as the current DHT reading algorithm adjusts itself to work on faster procs.
-DHT dht(DHTPIN, DHTTYPE);
-void setup() {
-  Serial.begin(9600);
-  Serial.println(F("DHTxx test!"));
+
+//DHT define
+#define DHTTYPE DHT11
+#define DHTPIN 14
+DHT dht(DHTPIN,DHTTYPE);
+
+//Define LED
+#define LED 27
+WidgetLED LED_ON_APP(V2);
+int button;
+
+char auth[] = BLYNK_AUTH_TOKEN;
+
+
+char ssid [] ="Thuy Nga";
+char pass []="nga0936258058";
+
+void setup(){
+
+  //Debug console
+  pinMode(LED,OUTPUT);
+  Serial.begin(115200);
   dht.begin();
+  
+  Blynk.begin(auth,ssid,pass);
 }
-void loop() {
-  // Wait a few seconds between measurements.
-  delay(2000);
-  // Reading temperature or humidity takes about 250 milliseconds!
-  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+
+BLYNK_WRITE(V3){
+  button = param.asInt();
+  if (button == 1){
+    digitalWrite(LED,HIGH);
+    LED_ON_APP.on();
+  } else {
+    digitalWrite(LED,LOW);
+    LED_ON_APP.off();
+  }
+}
+
+void loop()
+{
+  Blynk.run();
+  //Read Temp;
+  float t= dht.readTemperature();
+  //Read Humi
   float h = dht.readHumidity();
-  // Read temperature as Celsius (the default)
-  float t = dht.readTemperature();
-  // Read temperature as Fahrenheit (isFahrenheit = true)
-  float f = dht.readTemperature(true);
-  // Check if any reads failed and exit early (to try again).
-  if (isnan(h) || isnan(t) || isnan(f)) {
-    Serial.println(F("Failed to read from DHT sensor!"));
+  //Check isRead
+  if(isnan(h) || isnan(t)){
+    delay(500);
+    Serial.println("Failed to read from DHT sensor!\n");
     return;
   }
-  // Compute heat index in Fahrenheit (the default)
-  float hif = dht.computeHeatIndex(f, h);
-  // Compute heat index in Celsius (isFahreheit = false)
-  float hic = dht.computeHeatIndex(t, h, false);
-  Serial.print(F("Humidity: "));
-  Serial.print(h);
-  Serial.print(F("%  Temperature: "));
-  Serial.print(t);
-  Serial.print(F("°C "));
-  Serial.print(f);
-  Serial.print(F("°F  Heat index: "));
-  Serial.print(hic);
-  Serial.print(F("°C "));
-  Serial.print(hif);
-  Serial.println(F("°F"));
+
+  Blynk.virtualWrite(V0,t);
+  Blynk.virtualWrite(V1,h);
+
+  Serial.print("\n");
+  Serial.print("Humility: " + String(h) + "%");
+  Serial.print("\t");
+  Serial.print("Temperature: " + String(t) + "C");
+  delay(2000);
 }
